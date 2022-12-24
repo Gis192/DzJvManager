6d79547461f8855bf873a9b2058948e2b56166c7
Ivan Golikov <gis92@rambler.ru>
2022-12-24 17:06:51 +0300

    ready2

--- src/main/java/ru/netology/DzJvManager/MovieManager/MovieManager.java
+++ src/main/java/ru/netology/DzJvManager/MovieManager/MovieManager.java
@@ -1,12 +1,14 @@
+package ru.netology.DzJvManager.MovieManager;
+
 public class MovieManager {
     private String[] movies = new String[0];
     private int limit;
 
-    public MovieManager(){
-        this.limit =10;
+    public MovieManager() {
+        this.limit = 10;
     }
 
-    public MovieManager (int limit) {
+    public MovieManager(int limit) {
         this.limit = limit;
     }
 
@@ -24,14 +26,15 @@ public class MovieManager {
     }
 
     public String[] findLast() {
-       int countMovie;
-       if (movies.length < limit) {
-          countMovie = movies.length;
-       }
-       else {
-           countMovie = limit;
-       }
-       String[] tmp = new String[countMovie];
+        int countMovie;
+        if (movies.length < limit) {
+            countMovie = movies.length;
+        }
+        else {
+            countMovie = limit;
+        }
+
+        String[] tmp = new String[countMovie];
         for (int i = 0; i < tmp.length; i++) {
             tmp[i] = movies[movies.length - 1 - i];
         }
--- src/test/java/ru/netology/DzJvManager/MovieMngrTest/MovieMngrTest.java
+++ src/test/java/ru/netology/DzJvManager/MovieMngrTest/MovieMngrTest.java
@@ -1,17 +1,20 @@
+package ru.netology.DzJvManager.MovieMngrTest;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.netology.DzJvManager.MovieManager.MovieManager;
 
 public class MovieMngrTest {
 
     @Test
-    public void testAddMovie(){
+    public void testAddMovie() {
         MovieManager manager = new MovieManager();
 
         manager.movieAdd("Fast Furious 1");
         manager.movieAdd("Fast Furious 2");
         manager.movieAdd("Fast Furious 3");
 
-        String[] expected = { "Fast Furious 1", "Fast Furious 2", "Fast Furious 3"};
+        String[] expected = {"Fast Furious 1", "Fast Furious 2", "Fast Furious 3"};
         String[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
@@ -34,13 +37,14 @@ public class MovieMngrTest {
         manager.movieAdd("Fast and Furious 11");
         manager.movieAdd("Fast and Furious 12");
 
-        String[] expected = { "Fast and Furious 12", "Fast and Furious 11", "Fast and Furious 10",
+        String[] expected = {"Fast and Furious 12", "Fast and Furious 11", "Fast and Furious 10",
                 "Fast and Furious 9", "Fast and Furious 8", "Fast and Furious 7",
-                "Fast and Furious 6", "Fast and Furious 5", "Fast and Furious 4", "Fast and Furious 3" };
+                "Fast and Furious 6", "Fast and Furious 5", "Fast and Furious 4", "Fast and Furious 3"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void tetsFindLastBelowLimit() {
         MovieManager manager = new MovieManager();
@@ -55,9 +59,9 @@ public class MovieMngrTest {
         manager.movieAdd("Fast and Furious 11");
         manager.movieAdd("Fast and Furious 12");
 
-        String[] expected = { "Fast and Furious 12", "Fast and Furious 11", "Fast and Furious 10",
+        String[] expected = {"Fast and Furious 12", "Fast and Furious 11", "Fast and Furious 10",
                 "Fast and Furious 9", "Fast and Furious 8", "Fast and Furious 7",
-                "Fast and Furious 6", "Fast and Furious 5", "Fast and Furious 4" };
+                "Fast and Furious 6", "Fast and Furious 5", "Fast and Furious 4"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
